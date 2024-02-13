fd42cecc8d0afd63d4795208654a9cd462de396f
arthurbykov <136387066+arthurbykov@users.noreply.github.com>
2024-02-13 14:25:48 +1300

    Add files via upload

--- 01_convertor_gui_v1.py
+++ 01_convertor_gui_v1.py
@@ -48,16 +48,33 @@ class Converter:
                                         text="To Celsius",
                                         bg="#990099",
                                         fg=button_fg,
-                                        font=button_font)
-        self.to_celsius_button.grid(row=0, column=0)
+                                        font=button_font, width=12)
+        self.to_celsius_button.grid(row=0, column=0, padx=5, pady=5)
 
         self.to_fahrenheit_button = Button(
                                         self.button_frame,
                                         text="To Fahrenheit",
                                         bg="#009900",
                                         fg=button_fg,
-                                        font=button_font)
-        self.to_fahrenheit_button.grid(row=0, column=1)
+                                        font=button_font, width=12)
+        self.to_fahrenheit_button.grid(row=0, column=1, padx=5, pady=5)
+
+        self.to_help_button = Button(
+                                    self.button_frame,
+                                    text="Help / Info",
+                                    bg="#CC6600",
+                                    fg=button_fg,
+                                    font=button_font, width=12)
+        self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
+
+        self.to_history_button = Button(
+                                        self.button_frame,
+                                        text="History / Export",
+                                        bg="#004C99",
+                                        fg=button_fg,
+                                        font=button_font, width=12,
+                                        state=DISABLED)
+        self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
 
 
 # main routine
